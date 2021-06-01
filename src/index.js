cc581f00f2f01cb832134d9168f12a6a7aa0e483
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-06-01 11:55:06 +0300

    исправил

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import countriesList from './templates/countries-list.hbs';
 
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
+defaults.width = '450px';
 
-import { error } from '@pnotify/core';
-import { defaults } from '@pnotify/core';
-
-defaults.styling = 'material';
+import { defaults, error } from '@pnotify/core';
 
 const cardContainer = document.querySelector('.js-card-container');
 const input = document.querySelector('.form-control');
@@ -19,17 +17,16 @@ input.addEventListener('input', debounce(onInput, 500));
 
 function onInput(e) {
   const searchQuery = e.target.value;
-  const fatchName = API.fetchCountryByName(searchQuery);
 
   if (searchQuery.length < 1) {
     return onClear();
   }
-
+  const fatchName = API.fetchCountryByName(searchQuery);
   return fatchName.then(data => {
     if (data.length >= 2 && data.length <= 10) {
-      return fatchName.then(renderList);
+      renderList(data);
     } else if (data.length < 2) {
-      return fatchName.then(renderCountriCard);
+      renderCountriCard(data);
     } else if (data.length > 10) {
       const myError = error({
         text: 'Too many matches found. Please enter a more specific query!',
